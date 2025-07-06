54d5371bef7482baf7831fe25a6768d94c8eb3ed
Bhagyashri Tamrakar <bt7355711982@gmail.com>
2025-07-06 23:53:01 +0530

    updated

--- config/db.js
+++ config/db.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
+  console.log('Connecting to MongoDB with URI:', process.env.MONGO_URI);
   await mongoose.connect(process.env.MONGO_URI); 
+  console.log('MongoDB connected successfully!');
 };
 
-// Add this export
 module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
+
+// Load environment variables FIRST
+dotenv.config();
+
+// Now import other modules that depend on env vars
 const connectDB = require('./config/db');
 const userRoutes = require('./routes/user.routes');
 
-
-dotenv.config();
-connectDB();
+connectDB();  // Now MONGO_URI will be defined
 
 const app = express();
 app.use(cors());
@@ -14,7 +17,6 @@ app.use(express.json());
 
 // Routes
 app.use('/api/todos', require('./routes/todo.routes'));
-// Register route
 app.use('/api/users', userRoutes);
 
 app.get('/', (req, res) => {
