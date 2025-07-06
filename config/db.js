63db0cf28011ecc5f492ac3bc87d9f2f5c9edf08
Bhagyashri Tamrakar <bt7355711982@gmail.com>
2025-07-06 23:46:45 +0530

    updated

--- config/db.js
+++ config/db.js
@@ -1,5 +1,8 @@
-// config/db.js
 const mongoose = require('mongoose');
+
 const connectDB = async () => {
-  await mongoose.connect(process.env.MONGODB_URI); 
+  await mongoose.connect(process.env.MONGO_URI); 
 };
+
+// Add this export
+module.exports = connectDB;
