67a63949005f9038b50eda7497f305374958269d
Bhagyashri Tamrakar <bt7355711982@gmail.com>
2025-07-07 23:56:37 +0530

    update on url

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-  await mongoose.connect(process.env.DATABASE_URL); 
+  await mongoose.connect(process.env.MONGO_URI); // Changed from DATABASE_URL to MONGO_URI
   console.log('MongoDB connected successfully!');
 };
 
